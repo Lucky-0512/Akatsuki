d8912a760c3daa60cb5abc02e6bd6096ddc4a749
Lucky-0512 <cultdraw999.com>
2025-10-09 18:36:40 +0530

    added file requiremtns.txt and preparing base for initial deployment

--- app.py
+++ app.py
@@ -379,8 +379,10 @@ def hover_divs():
     
 # Run the app - on condition check.
 if __name__ == "__main__":
-    #app.run(debug=True)
-    socketio.run(app,debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
+
 
 
 
